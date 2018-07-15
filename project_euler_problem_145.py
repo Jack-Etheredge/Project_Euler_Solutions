5db10c989c01ea85803461190884adb82a7b2c4f
Jack-Etheredge <jack.etheredge@gmail.com>
2018-07-15 13:17:54 -0400

    added problem

---
+++ project_euler_problem_145.py
@@ -0,0 +1,23 @@
+# Some positive integers n have the property that the sum [ n + reverse(n) ] consists entirely of odd (decimal) digits. For instance, 36 + 63 = 99 and 409 + 904 = 1313. We will call such numbers reversible; so 36, 63, 409, and 904 are reversible. Leading zeroes are not allowed in either n or reverse(n).
+# 
+# There are 120 reversible numbers below one-thousand.
+# 
+# How many reversible numbers are there below one-billion (109)?
+
+maxnum = 10**9
+countnums = 0
+
+for i in range(10, maxnum+1):
+    checkodddigits = True
+    if int(str(i)[-1]) == 0:
+        checkodddigits = False
+    sumnum = i+int(str(i)[::-1])
+    for j in range(len(str(sumnum))):
+        if int(str(sumnum)[j]) % 2 == 0:
+            checkodddigits = False
+    if checkodddigits == True:
+        countnums += 1
+    if i%100==0:
+        print(round((i/maxnum)*100,2),"%")
+
+print(countnums)
